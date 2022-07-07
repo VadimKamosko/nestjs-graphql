45d5736a1610987f350a5af9fed4653d7ccec5c9
VadimKamosko <vkamosko@yandex.by>
2022-07-07 16:29:06 +0300

    hotfix: user path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ArtistModule } from './modules/artist/artist.module';
 import { GenreModule } from './modules/genre/genre.module';
 import { TrackModule } from './modules/track/track.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 import { BandModule } from './modules/band/band.module';
 import { FavouriteModule } from './modules/favourite/favourite.module';
 import { AlbumModule } from './modules/album/album.module';
